17aae0ea3bcadaefe9d709f82c8af15ad088e6da
Miguel Martín <alu0101209777@ull.edu.es>
2021-04-25 19:31:58 +0100

    tests with wrong values updated

--- tests/notes_manager.spec.ts
+++ tests/notes_manager.spec.ts
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 import { NotesManager } from '../src/notes_manager';
 import { Note } from '../src/note';
 import { KnownColors } from '../src/Interfaces/colored';
+import { InvalidColor, InvalidNote, InvalidUsernameError, RepeatedNoteError } from '../src/Errors';
 
 const chai = require('chai');
 const deepEqualInAnyOrder = require('deep-equal-in-any-order');
@@ -11,7 +12,7 @@ const deepEqualInAnyOrder = require('deep-equal-in-any-order');
 
 chai.use(deepEqualInAnyOrder);
 
-describe('NotesManager tests', () => {
+describe('NotesManager good values tests', () => {
   
   // Deletes Notes folder
   fs.rmSync('./tests/Notes', {
@@ -57,12 +58,81 @@ describe('NotesManager tests', () => {
     expect(manager.searchNote('Frank', 'FrankNote')).to.be.eql(firstNote);
   });
 
+  it('Edits a note', () => {
+    manager.editNote('Frank', 'FrankNote', {newTitle: 'NewFrankNote'});
+    expect(manager.searchNote('Frank', 'NewFrankNote')).to.be.eql(new Note('NewFrankNote', 'This is Frank\'s note', KnownColors.green));
+  });
+});
+
+describe('NotesManager wrong values tests', () => {
+  
+  // Deletes Notes folder
+  fs.rmSync('./tests/Notes', {
+    recursive: true,
+    force: true,
+  });
+  
+  const manager = new NotesManager('./tests/Notes');
+  const firstNote = new Note('FrankNote', 'This is Frank\'s note', KnownColors.green);
+  const secondNote = new Note('SecondNote', 'This is Frank\'s second note', KnownColors.blue);
+
+  it('Normalizes path', () => {
+    expect(manager.normalizePath('apath/')).to.be.eq('apath/');
+  });
+
+  it('Adding a repeated note produces an RepeatedNoteError', () => {
+    manager.addNote('Frank', firstNote);
+    let err: RepeatedNoteError = new RepeatedNoteError('');
+    try {
+      manager.addNote('Frank', firstNote);
+    } catch (error) {
+      err = error;
+    }
+    expect(err.toString()).to.be.eql(new RepeatedNoteError('./tests/Notes/Frank/FrankNote.json').toString());
+  });
+
+
+  it('Listing a non-existing users notes produces an error', () => {
+    let err: InvalidUsernameError = new InvalidUsernameError('');
+    try {
+      manager.listNotes('Bill');
+    } catch (error) {
+      err = error;
+    }
+    expect(err.toString()).to.eq(new InvalidUsernameError('Bill').toString());
+  });
+
+  it('Printing a non-existing note produces an error', () => {
+    let err = new InvalidNote('');
+    try {
+      manager.printNote('Frank', 'SecondNote');
+    } catch (error) {
+      err = error;
+    }
+    expect(err.toString()).to.be.eq(new InvalidNote('./tests/Notes/Frank/SecondNote.json').toString());
+  });
+
+  it('Removing a non-existing note produces an error', () => {
+    let err = new InvalidNote('');
+    try {
+      manager.removeNote('Frank', 'SecondNote');
+    } catch (error) {
+      err = error;
+    }
+    expect(err.toString()).to.be.eq(new InvalidNote('./tests/Notes/Frank/SecondNote.json').toString());
+  });
+
   it('Returns undefined when can\'t find a note', () => {
     expect(manager.searchNote('Frank', 'SecondNote')).to.be.undefined;
   });
 
-  it('Edits a note', () => {
-    manager.editNote('Frank', 'FrankNote', {newTitle: 'NewFrankNote'});
-    expect(manager.searchNote('Frank', 'NewFrankNote')).to.be.eql(new Note('NewFrankNote', 'This is Frank\'s note', KnownColors.green));
+  it('Editing a note with a wrong color produces an error', () => {
+    let err = new InvalidNote('');
+    try {
+      manager.editNote('Frank', 'FrankNote', {newColor: 'purple'});
+    } catch (error) {
+      err = error;
+    }
+    expect(err.toString()).to.be.eq(new InvalidColor('purple').toString());
   });
 });
